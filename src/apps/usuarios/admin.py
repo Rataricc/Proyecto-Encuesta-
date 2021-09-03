d9b2193c232c71128b41a1565a374e5d89d37d8e
Charly_Sam20 <89050737+SamHman@users.noreply.github.com>
2021-09-03 16:20:54 -0300

    Proyecto Principal
    Agrego actualización del proyecto con todos los avances realizados

---
+++ src/apps/usuarios/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+
+
+# Register your models here.
+
+# me quede en el video en la parte o hora: 1:42:04 de Django apps + sttings + models
+
+
---
+++ src/encuesta/settings/local.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DATABASES = {
+   'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'encuesta',
+        'USER': 'mrHackman',
+        'PASSWORD': 'testNdriver',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
